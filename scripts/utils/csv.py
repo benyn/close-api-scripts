dd18846bc59b3f17111ab128f84a33291750886d
benyn <105991837+benyn@users.noreply.github.com>
2024-02-10 23:18:57 -0800

    update: add type hints to write_csv

--- scripts/utils/csv.py
+++ scripts/utils/csv.py
@@ -1,7 +1,8 @@
 import csv
+from typing import Any
 
 
-def write_csv(file_name, keys, rows):
+def write_csv(file_name: str, keys: list[str], rows: dict[str, Any]):
     with open(file_name, "w", newline="", encoding="utf-8") as f:
         writer = csv.DictWriter(f, keys)
         writer.writeheader()
